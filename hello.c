8459c028a8a22f0abca0b11bd0dbb774b49374d7
Владислав Кузнецов <61275998+Krokovlad@users.noreply.github.com>
2022-11-17 16:54:04 +0300

    added hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+    // no dependency from vfs_entity when using stderr
+    fprintf(stderr,"Hello from KasperskyOS!\n");
+
+    return EXIT_SUCCESS;
+}
